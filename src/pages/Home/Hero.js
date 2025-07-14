59d310bf13a085c4a31e618bdcc500a9ddb263bb
RehamRaed <r409257797@gmail.com>
2025-07-14 22:37:55 +0300

    edit hero section

--- src/pages/Home/Hero.js
+++ src/pages/Home/Hero.js
@@ -1,38 +1,69 @@
-import react from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import man from '../../assest/man.png';
+import man from "../../assest/man.png";
 import { Container, Row, Col } from "react-bootstrap";
+import { motion } from "framer-motion";
 import "../../styles/Hero.css";
+
 function Hero() {
   return (
     <section id="hero_section">
       <Container>
         <Row className="row">
           <Col lg={6} md={8} sm={12} className="col_1">
-            <h3 className="title">
-              {" "}
+            <motion.h3
+              className="title"
+              initial={{ y: -50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.6, delay: 0.2 }}
+            >
               we promote your <br />
               <span> busnisses</span>
-            </h3>
-            <p>
+            </motion.h3>
+
+            <motion.p
+              initial={{ y: -50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.6, delay: 0.4 }}
+            >
               We are dedicated to helping your business grow and reach new
               heights through tailored promotional strategies and innovative
               solutions
-            </p>
-            <div className="btn-wrapper">
+            </motion.p>
+
+            <motion.div
+              className="btn-wrapper"
+              initial={{ y: -50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.6, delay: 0.6 }}
+            >
               <Link className="btn" to="/">
                 Get Started
               </Link>
-            </div>
+            </motion.div>
           </Col>
-          <Col lg={6} md={6} sm={12}className="col_2">
-          <div className="hexagon-bg"></div>
-          <img src={man} alt="Man with Laptop" className="man-img" />
-            
+
+          <Col lg={6} md={6} sm={12} className="col_2">
+            <motion.div
+              className="hexagon-bg"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.8, delay: 0.2 }}
+            ></motion.div>
+
+            <motion.img
+              src={man}
+              alt="Man with Laptop"
+              className="man-img"
+              initial={{ scale: 0.5, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.7, delay: 0.4 }}
+            />
           </Col>
         </Row>
       </Container>
     </section>
   );
 }
+
 export default Hero;
